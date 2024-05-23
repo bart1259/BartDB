b8d4f7910e12a8650ce3ff08bfc7a4edbe86f30f
bartek <bartg1259@gmail.com>
2024-05-23 16:33:45 -0500

    Configres cmakefiles to build and link libs

---
+++ src/btreelib/b_tree.h
@@ -0,0 +1,6 @@
+#ifndef B_TREE_H
+#define B_TREE_H
+
+int add(int a, int b);
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "b_tree.h"
+
+int main()
+{
+    std::cout << "Starting tests.." << add(4,5) << std::endl;
+    return 0;
+}
